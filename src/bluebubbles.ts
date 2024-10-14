95c4146826ac32b848a41d0e9d0af8daf00422ae
Clansty <i@gao4.pw>
2024-10-14 19:52:39 +0800

    feat: auto refresh

---
+++ src/bluebubbles.ts
@@ -0,0 +1,12 @@
+import { createLogg } from "@guiiai/logg";
+
+const log = createLogg("BlueBubbles").useGlobalConfig();
+
+
+export default {
+  async requestRefreshLocations() {
+    const res = await fetch(`${process.env.BLUEBUBBLES_HOST}/api/v1/icloud/findmy/friends/refresh?password=${process.env.BLUEBUBBLES_PASS}`);
+
+    log.log("Refresh request sent", res.status);
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import { Format, LogLevel, setGlobalFormat, setGlobalLogLevel } from '@guiiai/logg'
 import mitm from "./mitm";
 import mqtt from "./mqtt";
+import bluebubbles from "./bluebubbles";
+import cron from 'node-cron'
 
 setGlobalLogLevel(LogLevel.Debug)
 setGlobalFormat(Format.Pretty)
 
 mitm.start()
 mqtt.publishAutoDiscoveryBase()
+
+cron.schedule("*/10 * * * *", bluebubbles.requestRefreshLocations)
